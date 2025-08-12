ae355a8fcd5a11f0860dba4b831eecbd7f2045af
Lepplex <lepplexalpha9@gmail.com>
2025-08-12 20:18:47 +0200

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,3 +1,45 @@
-wget https://batodisc.xyz/install.sh
-chmod +x install.sh
-./install.sh
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}=====================================================${NC}"
+echo -e "${YELLOW}              Batocera Disk Player Setup           ${NC}"
+echo -e "${BLUE}=====================================================${NC}"
+
+echo -e "Welcome to the Batocera Disk Player setup !"
+echo -e "What do you want to do ?"
+
+
+PS3="Your choice (choose a number) :"
+options=("Install BDP" "Uninstall BDP" "Quitter")
+# Menu
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Install BDP")
+            echo "Laucnhing installation script..."
+            wget https://batodisc.xyz/install.sh
+            chmod +x install.sh
+            bash install.sh
+            break
+            ;;
+        "Vérifier l'état du système")
+            echo "Vérification de l'état du système..."
+            wget https://batodisc.xyz/uninstall.sh
+            chmod +x uninstall.sh
+            bash uninstall.sh
+            break
+            ;;
+        "Quitter")
+            echo "Goodbye !"
+            break
+            ;;
+        *) 
+            # In case of an error
+            echo "That's not an option !"
+            ;;
+    esac
+done
